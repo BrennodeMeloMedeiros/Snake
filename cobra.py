9c935a221a335a288f19048dc64bc815450b0b47
Brenno <brennoprogmm@gmail.com>
2025-07-14 02:19:48 -0300

    Change to OOP

--- cobra.py
+++ cobra.py
@@ -1,10 +1,22 @@
-map_dimensions = (10,20)
 
-def CreateMap(map_width = 20, map_height = 40):
-    game_map = [["#" for x in range(map_width)] for y in range(map_height)] 
-    print(game_map)
 
-def ShowMap(map):
-    for line in map
-    
-ShowMap(CreateMap(*map_dimensions))
+class Game:
+    def __init__(self):
+        self.map_width = 10
+        self.map_height = 10
+        self.game_map = [["~" for x in range(self.map_width)] for y in range(self.map_height)] 
+
+    def ShowMap(map):
+        print()
+        print()
+        print()
+        for line in map:
+            line_drawing = ""
+            for column in line:
+                line_drawing += column
+            print(line_drawing)
+            
+game = Game()
+print(game.game_map)
+map = game.game_map
+game.ShowMap(map)
