86108cf23d64becdfe5320d3930e541d0a8c87de
Brenno <brennoprogmm@gmail.com>
2025-07-13 22:52:47 -0300

    Initial map creation

--- cobra.py
+++ cobra.py
@@ -1,1 +1,10 @@
 map_dimensions = (10,20)
+
+def CreateMap(map_width = 20, map_height = 40):
+    game_map = [["#" for x in range(map_width)] for y in range(map_height)] 
+    print(game_map)
+
+def ShowMap(map):
+    for line in map
+    
+ShowMap(CreateMap(*map_dimensions))
