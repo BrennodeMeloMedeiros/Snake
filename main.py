9653a376bbc07d6240cce04ff14ef899a858e005
Brenno <brennoprogmm@gmail.com>
2025-07-17 23:03:21 -0300

    Fruit spawn corrected

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ class Main:
         self.ifc = GameInterface()
     
     def loop(self):
-        self.game_map = self.game_map.spawn_fruit()
-        self.ifc.show_map(self.game_map)
+        print("Loop iniciated")
+        self.game_map.spawn_fruit()
+        self.ifc.show_map(self.game_map.grid)
 
 
 main = Main()
 main.loop()
 main.loop()
+
+# run = True
+# c = 0
+# while run:
+#     c += 1
+#     main.loop
+#     if c == 10:
+#         run = False
--- map_class.py
+++ map_class.py
@@ -32,9 +32,7 @@ class Map:
         fruit_block_x = fruit_block[0]
         fruit_block_y = fruit_block[1]
         self.grid[fruit_block_x][fruit_block_y] = self.fruit_block_char
-
         print(f"Fruit spawned at {fruit_block}")
-        return self.grid
 
 
 # game_map = Map()
